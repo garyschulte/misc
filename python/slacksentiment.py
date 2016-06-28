8b62225894584ec2e6dac1ded241d4c4be1072c4
Gary Schulte <gschulte@marinsoftware.com>
2016-06-28 02:56:09 -0700

    add hackday python slack sentiment analysis stuff

---
+++ python/slacksentiment.py
@@ -0,0 +1,42 @@
+
+#!/usr/bin/env python
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk import tokenize
+from slacker import Slacker
+from elasticsearch import Elasticsearch
+import json
+import datetime
+
+slack = Slacker('xoxp-44709805366-44715811712-54834873393-803cb7c837')
+sid = SentimentIntensityAnalyzer()
+es = Elasticsearch('http://search-nltk-hackday-jarxkri3ge76dznvckb62i25hq.us-west-2.es.amazonaws.com:80')
+
+# get users in the #general channel so we can attribute names
+users={}
+
+for member in slack.users.list().body['members']:
+    users[member['id']] = member
+
+# get each channel
+for channel in slack.channels.list().body['channels']:
+
+    # get messages in each channel 
+    messages = slack.channels.history(channel=channel['id'], oldest="1465516800.000000").body['messages']
+
+    for message in messages:
+        timestamp = message['ts'].decode("utf-8").split('.')[0]
+        user = message['user']
+        sentences = tokenize.sent_tokenize(message['text'])
+        compoundSentiment = 0;
+        for sentence in sentences:
+            print(sid.polarity_scores(sentence))
+            compoundSentiment += sid.polarity_scores(sentence)['compound']/len(sentences)
+        message['sentiment'] = compoundSentiment
+        message['timestamp'] = datetime.datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+        message['username'] = users[user]['name']
+        message['channelid'] = channel['id']
+        message['channelname'] = channel['name']
+        #message['fullname'] = users[user]['first_name']+" "+users[user]['last_name']
+        print json.dumps(message)
+        es.index(index='nltkdemo', doc_type="slack", id=message['ts'], body=message)
